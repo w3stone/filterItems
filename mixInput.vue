3848fd36a63b0006c2a40fbaafb6e8de42855c90
w3stone <wjl.1993@gmail.com>
2018-12-28 17:49:32 +0800

    20181228

--- mixInput.vue
+++ mixInput.vue
@@ -9,9 +9,9 @@
                 placeholder="请输入内容"
                 @select="selected">
             </el-autocomplete>
-            <div class="control_box" v-show="xd">
-                <i class="el-icon-circle-plus" @click="addFn(index)"></i>
-                <i class="el-icon-remove" @click="delFn(index)"></i>
+            <div class="control_box">
+                <i class="el-icon-circle-plus" @click="add(index)"></i>
+                <i class="el-icon-remove" v-show="inputModel.length>1" @click="del(index)"></i>
             </div>
         </div>
     </div>
@@ -31,22 +31,33 @@
             }
         },
         methods: {
-            addFn(index){
-                this.inputModel.splice(index+1, 0, "");
+            add(index){
+                this.inputModel.splice(index+1, 0, ""); //从index+1索引开始插入
             },
-            delFn(index){
-                if(this.inputModel.length > 1)
+            del(index){
+                if(this.inputModel.length>1)
                     this.inputModel.splice(index, 1); //删除当前节点
             },
-            querySearch(queryString, cb) {
-                let options = this.options;
+            //搜索方法
+            querySearch(queryString, callback) {
+                let options = this.options || [];
                 let results = queryString ? options.filter(o=>{ return o.value.indexOf(queryString)!=-1 }): options;
                 // 调用 callback 返回建议列表的数据
-                cb(results);
+                callback(results);
             },
             selected(data){
                 //console.log(data);
                 this.$emit("selected", data);
+            },
+            //去除数组空元素
+            removeEmpty(list){
+                for(var i=0; i<list.length; i++){
+                    if(list[i]=="" || typeof(list[i])=="undefined"){
+                        list.splice(i,1);
+                        i--;
+                    }
+                }
+                return list;
             }
         },
         watch:{
@@ -57,10 +68,10 @@
                 }
             },
             inputModel:{ //监测自己变化，向父组件传值
-                immediate: true,
+                //immediate: true,
                 handler(newVal, oldVal){
-                    let finalArray = newVal.deepCopy().noEmpty(); //数组深复制
-                    //console.log(finalArray.join(","));
+                    let finalArray = JSON.parse(JSON.stringify(newVal));
+                    finalArray = this.removeEmpty(finalArray);
                     this.$emit("input", finalArray.join(","));
                     this.$emit("change", finalArray.join(","));
                 }
